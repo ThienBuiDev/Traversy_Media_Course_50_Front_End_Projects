44840ad2e9b2e3339c14c2ba1547b21bf6078b12
ThienBuiDev <tbui.vdev@gmail.com>
2022-05-11 16:19:53 -0500

    Rotating navigation - Done

--- Day3-Rotating Navigation/script.js
+++ Day3-Rotating Navigation/script.js
@@ -1,4 +1,10 @@
 const container = document.querySelector('.container')
-container.onclick = () => {
-	container.classList.toggle('show-nav')
+const open = document.getElementById('open')
+const close = document.getElementById('close')
+
+open.onclick = () => {
+	container.classList.add('show-nav')
+}
+close.onclick = () => {
+	container.classList.remove('show-nav')
 }
