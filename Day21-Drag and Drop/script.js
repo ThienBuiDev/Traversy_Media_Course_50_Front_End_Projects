004d509093c0152395bfb42ae8b19eb86477cd4f
ThienBuiDev <tbui.vdev@gmail.com>
2022-06-08 21:08:30 -0500

    Complet Drag N Drop

---
+++ Day21-Drag and Drop/script.js
@@ -0,0 +1,45 @@
+const fill = document.querySelector('.fill')
+const empties = document.querySelectorAll('.empty')
+
+fill.addEventListener('dragstart', dragStart)
+fill.addEventListener('dragend', dragEnd)
+
+for (const empty of empties) {
+	empty.addEventListener('dragover', dragOver)
+	empty.addEventListener('dragenter', dragEnter)
+	empty.addEventListener('dragleave', dragLeave)
+	empty.addEventListener('drop', dragDrop)
+}
+
+function dragStart() {
+	this.classList.add('hold')
+	setTimeout(() => {
+		this.classList.add('invisible')
+	}, 0)
+}
+
+function dragEnd(e) {
+	e.preventDefault()
+	e.target.style.cursor = 'default'
+	this.classList.remove('hold', 'invisible')
+}
+
+function dragOver(e) {
+	e.preventDefault()
+}
+
+function dragEnter(e) {
+	e.preventDefault()
+	this.classList.add('hovered')
+
+	console.log('enter')
+}
+
+function dragLeave() {
+	this.classList.remove('hovered')
+}
+
+function dragDrop() {
+	this.classList.remove('hovered')
+	this.append(fill)
+}
